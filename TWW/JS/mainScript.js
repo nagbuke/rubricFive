3efdef8a9aa177db31d4ac803f8c0917863a32c7
nagbuke <42808020+nagbuke@users.noreply.github.com>
2018-10-10 14:03:08 -0500

    Add files via upload
    Client-side dynamic page week 7 assignment

---
+++ TWW/JS/mainScript.js
@@ -0,0 +1,23 @@
+//JavaScript Document
+
+var tabButtons = document.querySelectorAll(".tabContainer .btnContainer button");
+var tabPanels = document.querySelectorAll(".tabContainer .tabPanel");
+
+function showPanel(panelIndex,colorCode) {
+	"use strict";
+	tabButtons.forEach(function(node){
+		node.style.backgroundColor = "";
+		node.style.color = "";
+	});
+	tabButtons[panelIndex].style.backgroundColor = colorCode;
+	tabButtons[panelIndex].style.color = "white";
+	
+	tabButtons.forEach(function(node){
+		node.style.display ="none";
+		
+	});
+	tabPanels[panelIndex].style.display = "block";
+	tabPanels[panelIndex].style.backgroundColor = colorCode;
+}
+
+//showPanel(0, '#f44336');
